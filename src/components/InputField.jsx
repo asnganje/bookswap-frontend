a9e6c20251f47f111a7e5b99a4096b1470e85e2f
asnganje <abd.nganje@gmail.com>
2026-01-01 21:45:09 +0300

    add reusable input field

---
+++ src/components/InputField.jsx
@@ -0,0 +1,24 @@
+const InputField = ({
+  label,
+  type="text",
+  value,
+  onChange,
+  placeHolder,
+  required = true
+}) => {
+  return(
+    <div className="flex flex-col gap-2">
+      <label className="text-sm text-muted">{label}</label>
+      <input
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeHolder}
+        required={required}
+        className="px-4 py-3 rounded-xl bg-slate-300 border border-white/10 text-white outline-none focus:ring-2 focus:ring-blue-300"
+      />
+    </div>
+  )
+}
+
+export default InputField;
