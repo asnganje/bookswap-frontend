cdd787ec496fab577e80b54eee81335e03490d49
asnganje <abd.nganje@gmail.com>
2026-01-02 09:12:03 +0300

    fix styling

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -6,11 +6,11 @@ const AuthForm = ({title, children, onSubmit, btnText, footer}) => {
       onSubmit={onSubmit}
       className="bg-gray-900 border border-white/10 rounded-3xl p-10 w-full max-w-md"
     >
-      <h1 className="text-3xl font-bold text-white mb-8">{title}</h1>
-      <div className="space-y-6">{children}</div>
+      <h1 className="text-3xl font-bold text-white mb-4">{title}</h1>
+      <div className="space-y-2">{children}</div>
       <button
         type="submit"
-        className="mt-8 w-full py-3 bg-blue-300 rounded-xl font-semibold hover:opacity-90 transition"
+        className="mt-8 w-full py-3 bg-blue-300 rounded-xl cursor-pointer font-semibold hover:opacity-90 transition"
       >
         {btnText}
       </button>
