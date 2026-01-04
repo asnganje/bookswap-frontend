210a9e6bf83645bc41161eedcd83a19aa728ae87
asnganje <abd.nganje@gmail.com>
2026-01-04 08:08:04 +0300

    add toast container to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 import Landing from "../src/pages/Landing"
 import Books from "../src/pages/Books"
 import Login from "../src/pages/Login"
@@ -7,26 +9,37 @@ import Signup from "../src/pages/Signup"
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path = "/"
-          element={<Landing/>}
-        />
-        <Route
-          path = "/books"
-          element={<Books />}
-        />
-        <Route
-          path = "/login"
-          element={<Login />}
-        />
-        <Route
-          path = "/register"
-          element={<Signup />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path = "/"
+            element={<Landing/>}
+          />
+          <Route
+            path = "/books"
+            element={<Books />}
+          />
+          <Route
+            path = "/login"
+            element={<Login />}
+          />
+          <Route
+            path = "/register"
+            element={<Signup />}
+          />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose="3000"
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
+    </>
   )
 }
 
