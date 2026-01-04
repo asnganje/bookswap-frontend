8f87ca1afc46325fdc457a6ad43169ce563524ab
asnganje <abd.nganje@gmail.com>
2026-01-04 06:50:43 +0300

    setup authReducer, slice and relevant auth thunks

---
+++ src/redux/slices/authSlice.js
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { loginUser, registerUser } from "../thunks/authThunk";
+
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user:null,
+    token:localStorage.get("token"),
+    loading: false,
+    msg:"",
+    error:null
+  },
+  reducers: {
+    logout(state) {
+      state.user = null,
+      state.token = null,
+      localStorage.removeItem("token")
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+    .addCase(registerUser.pending, (state)=> {
+      state.loading = true
+    })
+    .addCase(registerUser.fulfilled, (state, action)=> {
+      state.loading = false;
+      state.user = action.payload.user
+    })
+    .addCase(registerUser.rejected, (state, action)=> {
+      state.loading = false;
+      state.error = action.payload
+    })
+    .addCase(loginUser.pending, (state)=> {
+      state.loading = true;
+    })
+    .addCase(loginUser.fulfilled, (state, action)=> {
+      state.loading = false;
+      state.user = action.payload.user;
+      state.token = action.payload.token
+      state.msg = action.payload.msg
+    })
+    .addCase(loginUser.rejected, (state, action)=> {
+      state.loading = false;
+      state.error = action.payload
+    })
+  }
+})
+
+export const {logout} = authSlice.actions;
+const authReducer = authSlice.reducer
+export default authReducer
