a97c7f912bc8450242d356436366692826bfe62a
asnganje <abd.nganje@gmail.com>
2026-01-05 07:11:54 +0300

    Add relevant thunks for handling books

---
+++ src/redux/thunks/booksThunk.js
@@ -0,0 +1,57 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../../services/api";
+
+export const createBook = createAsyncThunk("books/createBook", 
+  async (payload, {rejectWithValue})=> {
+    try {
+      const response = await api.post("/books", payload)
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error?.response?.data)
+    }
+  }
+)
+
+export const getBook = createAsyncThunk("books/getBook", 
+  async (payload, {rejectWithValue})=> {
+    try {
+      const response = await api.get(`/books/${payload}`)
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error?.response?.data)
+    }
+  }
+)
+
+export const getBooks = createAsyncThunk("books/getBooks", 
+  async (_, {rejectWithValue})=> {
+    try {
+      const response = await api.get("/books")
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error?.response?.data)
+    }
+  }
+)
+
+export const updateBook = createAsyncThunk("books/updateBook", 
+  async ({data, id}, {rejectWithValue})=> {
+    try {
+      const response = await api.put(`/books/${id}`, data)
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error?.response?.data)
+    }
+  }
+)
+
+export const deleteBook = createAsyncThunk("books/deleteBook", 
+  async (payload, {rejectWithValue})=> {
+    try {
+      const response = await api.delete(`/books/${payload}`)
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error?.response?.data)
+    }
+  }
+)
